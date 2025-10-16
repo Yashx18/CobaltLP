3f2927a0d7ea7106bc8f9a7371c8d0b9bd326788
ken <yash20004x17@gmail.com>
2025-10-16 10:14:38 +0000

    added input for email

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -18,17 +18,24 @@ const Hero = () => {
       </svg>
       <div className="mx-auto w-full max-w-5xl pt-50">
         <div className="mx-auto my-0 flex h-full max-w-4xl flex-col items-center justify-center">
-          <h1 className="max-w-3xl bg-[linear-gradient(to_bottom_right,white,grey)] bg-clip-text text-center text-4xl font-bold sm:text-6xl md:text-7xl sm:font-semibold text-transparent">
+          <h1 className="max-w-3xl bg-[linear-gradient(to_bottom_right,white,grey)] bg-clip-text text-center text-4xl font-bold text-transparent sm:text-6xl sm:font-semibold md:text-7xl">
             Unleash the power of intuitive finance
           </h1>
-          <p className="mt-6 max-w-2xl text-center text-md sm:text-lg text-neutral-400">
+          <p className="text-md mt-6 max-w-2xl text-center text-neutral-400 sm:text-lg">
             Say goodbye to the outdated financial tools. Every small business owner, regardless of
             the background, can now manage their business like a pro. Simple. Intuitive. And never
             boring.
           </p>
         </div>
         <div className="mt-10 flex w-full flex-col gap-6">
-          <CTA />
+          <div className="z-100 mx-auto flex w-fit items-center gap-2">
+            <input
+              type="text"
+              className="rounded-full border border-neutral-400/20 px-4 py-2 text-sm text-neutral-200 backdrop-blur-2xl focus:outline-none focus:ring-2 focus:ring-transparent focus:ring-offset-2 focus:ring-offset-black focus:shadow-[0_0_0_3px]"
+              placeholder="name@gmail.com"
+            />
+            <CTA />
+          </div>
 
           <div className="group z-1 mx-auto flex h-auto cursor-pointer flex-col items-center justify-center gap-1 text-neutral-400 hover:text-neutral-100">
             <p>Learn more</p>
@@ -58,11 +65,11 @@ const Hero = () => {
         {/* Hero feature */}
         <div className="flex w-full grid-cols-2 flex-col items-start gap-2 pb-10 sm:py-10 md:py-20 lg:grid lg:gap-5">
           <div className="col-span-1 lg:pr-10">
-            <h2 className="tracking-light bg-[linear-gradient(to_bottom_right,white,grey)] bg-clip-text pb-2 text-2xl sm:text-3xl md:text-4xl font-bold text-transparent">
+            <h2 className="tracking-light bg-[linear-gradient(to_bottom_right,white,grey)] bg-clip-text pb-2 text-2xl font-bold text-transparent sm:text-3xl md:text-4xl">
               Who said finance has to be boring?
             </h2>
           </div>
-          <div className="col-span-1 w-full text-md sm:text-lg md:text-xl text-neutral-400">
+          <div className="text-md col-span-1 w-full text-neutral-400 sm:text-lg md:text-xl">
             <p>
               With Cobalt, managing your business finances is effortless, empowering, and never
               boring. Our intuitive platform brings clarity to your cash flow, simplifies your
