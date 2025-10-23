3e529042e02daa1875d86d2a142af9fec35131a7
ken <yash20004x17@gmail.com>
2025-10-23 13:24:58 +0000

    minor fix

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
           </div>
           {/*Social Links Section */}
           <div className="flex items-center gap-6 text-zinc-400">
-            <a href="x" className="transition-colors duration-200 hover:text-zinc-300">
+            <a href="/" className="transition-colors duration-200 hover:text-zinc-300">
               <svg
                 fill="currentColor"
                 viewBox="0 0 16 16"
@@ -49,7 +49,7 @@ const Footer = () => {
                 ></path>
               </svg>
             </a>
-            <a href="linkedIn" className="transition-colors duration-200 hover:text-zinc-300">
+            <a href="/" className="transition-colors duration-200 hover:text-zinc-300">
               <svg
                 fill="currentColor"
                 viewBox="0 0 24 24"
@@ -60,7 +60,7 @@ const Footer = () => {
                 <path d="M21 1.5H3c-.83 0-1.5.68-1.5 1.51V21c0 .83.67 1.51 1.5 1.51h18c.82 0 1.5-.68 1.5-1.51V3c0-.83-.68-1.51-1.5-1.51Zm-13.15 18H4.73V9.48h3.12V19.5ZM6.29 8.1a1.8 1.8 0 1 1 0-3.6 1.8 1.8 0 0 1 0 3.6Zm13.22 11.4h-3.1v-4.88c0-1.16-.03-2.65-1.63-2.65-1.62 0-1.87 1.26-1.87 2.57v4.96h-3.1V9.48h2.98v1.37h.04a3.28 3.28 0 0 1 2.95-1.62c3.15 0 3.73 2.08 3.73 4.78v5.49Z"></path>
               </svg>
             </a>
-            <a href="fb" className="transition-colors duration-200 hover:text-zinc-300">
+            <a href="/" className="transition-colors duration-200 hover:text-zinc-300">
               <svg
                 fill="currentColor"
                 viewBox="0 0 24 24"
